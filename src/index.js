77c3f8be43a5b650116c8b39583d1b58acbbb113
VitoZMX <Vi-69@yandex.ru>
2021-02-20 18:41:29 +0300

    feat: add code in file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+    if (number < 0) {
+        return [number * (-1)].toString().split('').reverse().join('');
+    } else {
+        return number.toString().split('').reverse().join('');
+    }
 }
